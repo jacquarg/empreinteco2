3adcf2aafb844ce7b0add57df15193b1d5c47359
Guillaume Jacquart <guillaume-externe.jacquart@edf.fr>
2019-06-26 09:46:36 +0200

    Add my transport emissions today.

--- README.md
+++ README.md
@@ -5,6 +5,13 @@
 [v] Camembert conso moyenne des français par "poste"
 * Objectif CO2 par individu
 [v] Ramené par an, mois, jour, ...
+
+
+
+* équivalence objectif, par catégories.
 * Personnalisation résultat transport
 * Personnalisation résultat électricité / chauffage
 * Personnalisation résultat alimentation ... ?
+
+* journée typique (travail) <-- transport, ...
+* conso énergie <-- d'après facture ... ?
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,12 @@
 
 
 const computeCategories = () => {
-  Object.values(data).forEach(v => v.total = computeCategory(v))
+  Object.values(data).forEach(v => {
+    v.total = computeCategory(v)
+    v.totalDaily = Math.floor(v.total / 365)
+    v.totalMonthly = Math.floor(v.total / 12)
+    v.today = v.totalDaily
+  })
   // const res = Object.keys(data).map(k => [k, computeCategory(data[k])])
   // console.log(res)
   // return res
@@ -33,15 +38,30 @@ const displayTable = () => {
   const buildCatRow = (cat) => {
     return `<tr>
     <th>${cat.label}</th>
-    <td>${Math.floor(cat.total / 365)}</td>
-    <td>${Math.floor(cat.total / 12)}</td>
+    <td>${cat.today}</td>
+    <td>${cat.totalDaily}</td>
+    <td>${cat.totalMonthly}</td>
     <td>${cat.total}</td>
     </tr>
     `
   }
 
-  $("tbody").append(buildCatRow({ label: "objectif", total: 2000 }))
-  $("tbody").append(buildCatRow({ label: "Total", total: 12000 }))
+  $("tbody").empty()
+
+  $("tbody").append(buildCatRow({ label: "objectif",
+    total: 2000,
+    today: Math.floor(2000 / 365),
+    totalDaily: Math.floor(2000 / 365),
+    totalMonthly: Math.floor(2000 / 12),
+
+  }))
+
+  $("tbody").append(buildCatRow({ label: "Total",
+    total: Object.values(data).reduce((agg, it) => agg + it.total, 0),
+    today: Object.values(data).reduce((agg, it) => agg + it.today, 0),
+    totalDaily: Object.values(data).reduce((agg, it) => agg + it.totalDaily, 0),
+    totalMonthly: Object.values(data).reduce((agg, it) => agg + it.totalMonthly, 0),
+   }))
 
   Object.values(data).forEach(it => {
     $("tbody").append(buildCatRow(it))
@@ -50,6 +70,7 @@ const displayTable = () => {
 
 
 const main = () => {
+  attachEvents()
   computeCategories()
   // var res = computeCategories()
 
@@ -66,5 +87,19 @@ const main = () => {
 
   displayTable()
 }
+
+const setTodayValue = (d) => {
+    data.transports.today = d / 1000
+
+}
+
+const attachEvents = () => {
+  $("button").click((ev) => {
+    const data = Number($("#transportsToday").val())
+    setTodayValue(data)
+    displayTable()
+  })
+}
+
 
 $(document).ready(() => main())
